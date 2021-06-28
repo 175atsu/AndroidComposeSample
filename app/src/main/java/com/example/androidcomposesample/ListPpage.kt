d6b9780d8476391c8e05c90c279b6c17cccee7de
Iino <atsuhiro_iino_xa@cyberagent.co.jp>
2021-06-29 02:07:44 +0900

    update version, fix coil

--- app/src/main/java/com/example/androidcomposesample/ListPpage.kt
+++ app/src/main/java/com/example/androidcomposesample/ListPpage.kt
@@ -1,6 +1,7 @@
 package com.example.androidcomposesample
 
 import androidx.compose.animation.animateColorAsState
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -19,106 +20,105 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.chrisbanes.accompanist.coil.CoilImage
+import com.google.accompanist.coil.rememberCoilPainter
 import kotlinx.coroutines.launch
 
 @Composable
 fun ListPage() {
-    val listSize = 100
-    val names = List(listSize) { "Hello Android #$it !" }
-    val countState = remember { mutableStateOf(0) }
-    val scrollState = rememberLazyListState()
+  val listSize = 100
+  val names = List(listSize) { "Hello Android #$it !" }
+  val countState = remember { mutableStateOf(0) }
+  val scrollState = rememberLazyListState()
 
-    Surface(
-        color = Color.Yellow
+  Surface(
+    color = Color.Yellow
+  ) {
+    Column(
+      modifier = Modifier.fillMaxHeight()
     ) {
-        Column(
-            modifier = Modifier.fillMaxHeight()
-        ) {
-            ControlButton(scrollState, listSize)
-            ListItem(scrollState, names, Modifier.weight(1f))
-            Counter2(count = countState.value, updateCount = { count -> countState.value = count })
-        }
+      ControlButton(scrollState, listSize)
+      ListItem(scrollState, names, Modifier.weight(1f))
+      Counter2(count = countState.value, updateCount = { count -> countState.value = count })
     }
+  }
 }
 
 @Composable
 fun ListItem(scrollState: LazyListState, names: List<String>, modifier: Modifier = Modifier) {
-    LazyColumn(
-        modifier = modifier,
-        state = scrollState
-    ) {
-        items(items = names) { name ->
-            val isSelected = remember { mutableStateOf(false) }
-            val backgroundColor =
-                animateColorAsState(targetValue = if (isSelected.value) Color.Red else Color.Transparent)
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                CoilImage(
-                    data = "https://developer.android.com/images/brand/Android_Robot.png",
-                    contentDescription = "Android Logo",
-                    modifier = Modifier.size(50.dp)
-                )
-                Text(
-                    text = name,
-                    modifier = modifier
-                        .padding(24.dp)
-                        .background(color = backgroundColor.value)
-                        .clickable { isSelected.value = !isSelected.value }
-                )
-            }
-            Divider(color = Color.Black)
-        }
+  LazyColumn(
+    modifier = modifier,
+    state = scrollState
+  ) {
+    items(items = names) { name ->
+      val isSelected = remember { mutableStateOf(false) }
+      val backgroundColor =
+        animateColorAsState(targetValue = if (isSelected.value) Color.Red else Color.Transparent)
+      Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+          painter = rememberCoilPainter("https://developer.android.com/images/brand/Android_Robot.png"),
+          contentDescription = "Android Logo"
+        )
+        Text(
+          text = name,
+          modifier = modifier
+            .padding(24.dp)
+            .background(color = backgroundColor.value)
+            .clickable { isSelected.value = !isSelected.value }
+        )
+      }
+      Divider(color = Color.Black)
     }
+  }
 }
 
 @Composable
 fun ControlButton(scrollState: LazyListState, listSize: Int) {
-    val coroutineScope = rememberCoroutineScope()
-    Row(
-        modifier = Modifier.fillMaxWidth()
+  val coroutineScope = rememberCoroutineScope()
+  Row(
+    modifier = Modifier.fillMaxWidth()
+  ) {
+    ScrollButton(
+      Modifier.weight(1f),
+      ButtonDefaults.buttonColors(Color.White),
+      "Scroll to the top"
     ) {
-        ScrollButton(
-            Modifier.weight(1f),
-            ButtonDefaults.buttonColors(Color.White),
-            "Scroll to the top"
-        ) {
-            coroutineScope.launch {
-                scrollState.animateScrollToItem(0)
-            }
-        }
-        ScrollButton(
-            Modifier.weight(1f),
-            ButtonDefaults.buttonColors(Color.Red),
-            "Scroll to the end"
-        ) {
-            coroutineScope.launch {
-                scrollState.animateScrollToItem(listSize - 1)
-            }
-        }
+      coroutineScope.launch {
+        scrollState.animateScrollToItem(0)
+      }
     }
+    ScrollButton(
+      Modifier.weight(1f),
+      ButtonDefaults.buttonColors(Color.Red),
+      "Scroll to the end"
+    ) {
+      coroutineScope.launch {
+        scrollState.animateScrollToItem(listSize - 1)
+      }
+    }
+  }
 }
 
 @Composable
 fun ScrollButton(
-    modifier: Modifier = Modifier,
-    color: ButtonColors,
-    label: String,
-    click: () -> Unit
+  modifier: Modifier = Modifier,
+  color: ButtonColors,
+  label: String,
+  click: () -> Unit
 ) {
-    Button(
-        onClick = click,
-        modifier = modifier.padding(8.dp),
-        colors = color,
-    ) {
-        Text(
-            text = label,
-            style = typography.subtitle1,
-        )
-    }
+  Button(
+    onClick = click,
+    modifier = modifier.padding(8.dp),
+    colors = color,
+  ) {
+    Text(
+      text = label,
+      style = typography.subtitle1,
+    )
+  }
 }
 
 @Preview("ListPage")
 @Composable
 fun PreviewListPage() {
-    ListPage()
+  ListPage()
 }
---
+++ buildSrc/src/main/java/com/example/androidcomposesample/buildsrc/Versions.kt
@@ -0,0 +1,6 @@
+package com.example.androidcomposesample.buildsrc
+
+object Versions {
+  const val kotlin = "1.5.10"
+  const val ktlint = "0.40.0"
+}
